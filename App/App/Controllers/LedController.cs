ec94373552252758d26f59467fbf899739c68c4e
Megamabus <faacundo.brlz@gmail.com>
2020-09-28 23:10:14 -0300

    Cambios Para revisar.

---
+++ App/App/Controllers/LedController.cs
@@ -0,0 +1,41 @@
+﻿using App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace App.Controllers
+{
+    public class LedController : Controller
+    {
+
+
+        // GET: Led
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ModificarLed(NotificationViewModel RGB)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44368/api/Notification");
+                var postJob = client.PostAsJsonAsync<NotificationViewModel>("notification", RGB);
+                postJob.Wait();
+
+                var postResult = postJob.Result;
+                if (postResult.IsSuccessStatusCode)
+                    return RedirectToAction("Index");
+            }
+              
+            return View("Index");
+        }
+    }
+}
