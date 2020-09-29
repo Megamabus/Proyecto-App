0ead65b271a9d3b3e4a1ca911b43c44464d1e9aa
Megamabus <faacundo.brlz@gmail.com>
2020-09-29 05:43:15 -0300

    Rutas al js d ela vista y path para las llamadas a la api y al controller.

--- App/App/Controllers/LedController.cs
+++ App/App/Controllers/LedController.cs
@@ -12,21 +12,28 @@ namespace App.Controllers
 {
     public class LedController : Controller
     {
+        //public string routeControllerPath = "http://192.168.0.13/Led/ModificarLed";//ruta para usar cuando s publica
+        public string routeControllerPath = "https://localhost:44354/Led/ModificarLed";//ruta de debug para prebas
+        //String serviceArduinoPath = "http://192.168.0.13:70/api/Notification";//ruta para usar cuando s publica
+        public string serviceArduinoPath = "https://localhost:44324/api/Notification";//ruta de debug para prebas
 
-
+        
         // GET: Led
         [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            ViewBag.routeControllerPath = routeControllerPath;
+            return View(ViewBag);
         }
 
         [HttpPost]
         public ActionResult ModificarLed(NotificationViewModel RGB)
         {
+            
+
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("http://192.168.0.13:70/api/Notification");
+                client.BaseAddress = new Uri(serviceArduinoPath);
                 var postJob = client.PostAsJsonAsync<NotificationViewModel>("notification", RGB);
                 postJob.Wait();
 
