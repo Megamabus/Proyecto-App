535680fee9d35f54b7992731bd7f6fbc3be2f1f7
Megamabus <faacundo.brlz@gmail.com>
2020-09-30 19:25:03 -0300

    Agregue un nuevo scketch d arduino

---
+++ App/SketchArduino/SketchArduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SketchArduino.ino
+ Created:	30/09/2020 19:17:57
+ Author:	facundo
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
