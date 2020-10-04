daa17d7c69ed140989541db9c90714f3ff722154
Megamabus <faacundo.brlz@gmail.com>
2020-10-04 06:11:05 -0300

    Instalamos el .core por el [frombody] que alfinal no usamos. Agregue el controler de SetearRele. LLamada por ajax para setear rele. Agregue boton agregar rele. Cambie el controler led y ahora se llama controlArduino.

--- App/App/App_Start/RouteConfig.cs
+++ App/App/App_Start/RouteConfig.cs
@@ -18,6 +18,7 @@ namespace App
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
